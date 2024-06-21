73129823b23a74edee0f595b44ddb81eb90066e2
Shubham <nikamshubham224@gmail.com>
2024-06-21 00:27:26 -0700

    Configure the store with cartReducer under the cart state slice.,Enable Redux DevTools for development debugging

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
 
+//Import configureStore from Redux Toolkit.
+//Import cartReducer from the cart slice.
+//Create and export the Redux store:
+//  slice.
+//Enable Redux DevTools for development debugging.
+
+
+
+
+
+
+
+
+
+
+
 export const store = configureStore({
     reducer: {
       cart: cartReducer,
